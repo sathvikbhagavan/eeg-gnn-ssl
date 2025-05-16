5ac1b84072ee5566fc3103b9f91431d050242469
Sajal Chaurasia <35844205+sajal09@users.noreply.github.com>
2025-05-16 22:08:39 +0200

    Update utils.py with Distance Adj Matrix Calculation

--- utils.py
+++ utils.py
@@ -493,3 +493,36 @@ def compute_regression_loss(
         return masked_mae_loss(y_predicted, y_true, mask_val=mask_val)
     else:
         return masked_mse_loss(y_predicted, y_true, mask_val=mask_val)
+
+def get_adjacency_matrix(distance_df, sensor_ids, dist_k=0.9):
+    """
+    Args:
+        distance_df: data frame with three columns: [from, to, distance].
+        sensor_ids: list of sensor ids.
+        dist_k: threshold for graph sparsity
+    Returns:
+        adj_mx: adj
+    """
+    num_sensors = len(sensor_ids)
+    dist_mx = np.zeros((num_sensors, num_sensors), dtype=np.float32)
+    dist_mx[:] = np.inf
+    # Builds sensor id to index map.
+    sensor_id_to_ind = {}
+    for i, sensor_id in enumerate(sensor_ids):
+        sensor_id_to_ind[sensor_id] = i
+
+    # Fills cells in the matrix with distances.
+    for row in distance_df.values:
+        if row[0] not in sensor_id_to_ind or row[1] not in sensor_id_to_ind:
+            continue
+        dist_mx[sensor_id_to_ind[row[0]], sensor_id_to_ind[row[1]]] = row[2]
+
+    # Calculates the standard deviation as theta.
+    distances = dist_mx[~np.isinf(dist_mx)].flatten()
+    std = distances.std()
+
+    # Sets entries that lower than a threshold, i.e., k, to zero for sparsity.    
+    adj_mx = np.exp(-np.square(dist_mx / std))
+    adj_mx[dist_mx > dist_k] = 0
+   
+    return adj_mx, sensor_id_to_ind
